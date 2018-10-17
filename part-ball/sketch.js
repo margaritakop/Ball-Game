be4e35a305b22d67b68e90ffce6db7f386d01adf
margaritakop <margarita.kopniczky@gmail.com>
2018-10-17 21:06:25 +0100

    set speed to 0 when ball touches goal

--- part-ball/sketch.js
+++ part-ball/sketch.js
@@ -4,16 +4,23 @@ var xspeed = 2
 var yspeed = 2
 var xedge = 400
 var yedge = 400
+var xgoal = 100
+var ygoal = 100
 
 function setup() {
-  createCanvas(xedge, yedge)
+  createCanvas(xedge, yedge);
 }
 
 function draw() {
   background(0)
   x = moveBall(x, y)[0]
   y = moveBall(x, y)[1]
+  if (Math.abs(x-xgoal) < 10 && Math.abs(y-ygoal) < 10){
+    xspeed = 0
+    yspeed = 0
+  }
   ellipse(x, y, 10, 10)
+  ellipse(xgoal, ygoal, 20, 20)
 }
 
 function ballShouldBounce (position, edge){
